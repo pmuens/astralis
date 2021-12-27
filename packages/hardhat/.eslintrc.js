9f4141557bad72ad69265a366bbbf7278b1b9d54
Philipp Muens <raute1337@gmx.de>
2021-12-27 08:44:47 +0100

    Setup TypeScript

--- packages/hardhat/.eslintrc.js
+++ packages/hardhat/.eslintrc.js
@@ -2,7 +2,22 @@ module.exports = {
   env: {
     node: true
   },
-  extends: ['eslint:recommended', 'prettier'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier'
+  ],
+  overrides: [
+    {
+      files: ['*.ts'],
+      parserOptions: {
+        project: 'tsconfig.json'
+      }
+    }
+  ],
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
   root: true,
   rules: {
     'no-console': 1
