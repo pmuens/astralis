1810f9c003c82de2c1004303eb5392b202a77e2b
Philipp Muens <raute1337@gmx.de>
2021-12-27 08:15:36 +0100

    Setup Prettier

---
+++ packages/hardhat/.prettierrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  arrowParens: 'always',
+  bracketSpacing: true,
+  endOfLine: 'auto',
+  printWidth: 120,
+  singleQuote: true,
+  semi: false,
+  tabWidth: 2,
+  useTabs: false,
+  trailingComma: 'none'
+}
