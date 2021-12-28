e68d58d00622594eba18d9f994a77b1bde50b671
Philipp Muens <raute1337@gmx.de>
2021-12-28 15:45:21 +0100

    Setup Next SEO

--- packages/next/pages/_app.tsx
+++ packages/next/pages/_app.tsx
@@ -1,8 +1,18 @@
 import '../styles/globals.css'
+import { DefaultSeo } from 'next-seo'
 import type { AppProps } from 'next/app'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <DefaultSeo
+        title="Next.js Template for EVM-based dApps"
+        description="The Next.js Template for EVM-based dApps."
+        titleTemplate="%s - Next.js Template for EVM-based dApps"
+      />
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
