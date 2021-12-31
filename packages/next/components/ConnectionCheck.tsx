1a27805245cc6a53e4ccb24445f7577951a0c777
Philipp Muens <raute1337@gmx.de>
2021-12-31 15:21:12 +0100

    Implement ConnectionCheck component

---
+++ packages/next/components/ConnectionCheck.tsx
@@ -0,0 +1,14 @@
+import useConnectionInfo from '../hooks/useConnectionInfo'
+
+export default function ConnectionCheck() {
+  const { isCorrectConnection, readChainName, writeChainName } = useConnectionInfo()
+  return (
+    <>
+      {!isCorrectConnection && (
+        <p>
+          You are connected to <u>{writeChainName}</u> rather than <u>{readChainName}</u>
+        </p>
+      )}
+    </>
+  )
+}
