e408c420941c6e241311d2dbac4662168c528f32
Philipp Muens <raute1337@gmx.de>
2022-01-01 12:00:51 +0100

    Refactor DecrementCounter component

---
+++ packages/next/components/Decrement.tsx
@@ -0,0 +1,31 @@
+import { useContractFunction } from '@usedapp/core'
+import { Dispatch, SetStateAction, useEffect } from 'react'
+
+import { getContractInfo } from '../utils/main'
+import { useSharedState } from '../utils/SharedState'
+
+export default function Decrement(props: Props) {
+  const { isLoading, setIsLoading } = props
+  const { contract } = getContractInfo()
+  const { setErrorMessage } = useSharedState()
+  const { state, send } = useContractFunction(contract, 'decrement')
+
+  useEffect(() => {
+    if (state.status == 'Mining') setIsLoading(true)
+    if (state.status != 'Mining') setIsLoading(false)
+    if (state.status == 'Fail' || state.status == 'Exception') {
+      setErrorMessage(state.errorMessage)
+    }
+  }, [state, setIsLoading, setErrorMessage])
+
+  return (
+    <button onClick={() => send()} disabled={isLoading}>
+      -
+    </button>
+  )
+}
+
+type Props = {
+  isLoading: boolean
+  setIsLoading: Dispatch<SetStateAction<boolean>>
+}
