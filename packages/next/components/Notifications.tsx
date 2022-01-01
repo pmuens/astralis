41718d1bbf03591909e6777bf6a4e06845946aaa
Philipp Muens <raute1337@gmx.de>
2022-01-01 12:04:49 +0100

    Refactor Notifications component

--- packages/next/components/Notifications.tsx
+++ packages/next/components/Notifications.tsx
@@ -4,13 +4,20 @@ export default function Notifications() {
   const { notifications } = useNotifications()
 
   const items = notifications.map((item) => {
-    return <li key={item.id}>{item.type}</li>
+    return (
+      <li key={item.id}>
+        <p>{prettifyText(item.type)}</p>
+      </li>
+    )
   })
 
-  return (
-    <section>
-      <h2>Notifications</h2>
-      {items.length ? <ol>{items}</ol> : <p>No Notifications</p>}
-    </section>
-  )
+  return items.length ? <ol>{items}</ol> : <p>-</p>
+}
+
+function prettifyText(text: string): string {
+  let result
+  result = text.charAt(0).toLowerCase() + text.slice(1)
+  result = result.replace(/([A-Z])/g, ' $1')
+  result = result.charAt(0).toUpperCase() + result.slice(1)
+  return result
 }
