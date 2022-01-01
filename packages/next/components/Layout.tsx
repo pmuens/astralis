722211f5874a8fa87a5bea5b4b59cbd46bc3ae35
Philipp Muens <raute1337@gmx.de>
2022-01-01 12:10:23 +0100

    Refactor Layout component

--- packages/next/components/Layout.tsx
+++ packages/next/components/Layout.tsx
@@ -4,10 +4,10 @@ import Footer from './Footer'
 
 export default function Layout({ children }: React.PropsWithChildren<unknown>) {
   return (
-    <div>
+    <>
       <Header />
       <Main>{children}</Main>
       <Footer />
-    </div>
+    </>
   )
 }
