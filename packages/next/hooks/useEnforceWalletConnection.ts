f5d456b50100e85094d5520071046558d54958b5
Philipp Muens <raute1337@gmx.de>
2022-01-07 20:13:47 +0100

    Display info when wallet connection is required

--- packages/next/hooks/useEnforceWalletConnection.ts
+++ packages/next/hooks/useEnforceWalletConnection.ts
@@ -2,14 +2,17 @@ import { useEffect } from 'react'
 import { useRouter } from 'next/router'
 
 import useConnectionInfo from './useConnectionInfo'
+import { useSharedState } from '../utils/SharedState'
 
 export default function useEnforceWalletConnection(redirectUrl: string) {
   const router = useRouter()
   const { isConnected } = useConnectionInfo()
+  const { setNotification } = useSharedState()
 
   useEffect(() => {
     if (router.isReady && !isConnected) {
+      setNotification({ message: 'You need to connect your Wallet to continue', type: 'info' })
       router.push(redirectUrl)
     }
-  }, [isConnected, router, redirectUrl])
+  }, [isConnected, router, redirectUrl, setNotification])
 }
