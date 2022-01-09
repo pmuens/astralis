0dbb6067541b71915ad3a798574843706b07d0c2
Philipp Muens <raute1337@gmx.de>
2022-01-09 12:08:31 +0100

    Update Next config to lint custom directories

--- packages/next/next.config.js
+++ packages/next/next.config.js
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
-  reactStrictMode: true
+  reactStrictMode: true,
+  eslint: {
+    dirs: [
+      // Default Next.js setup
+      //  (see: https://nextjs.org/docs/basic-features/eslint#linting-custom-directories-and-files)
+      'components',
+      'pages',
+      'lib',
+      // Custom directories
+      'hooks',
+      'scripts',
+      'utils'
+    ]
+  }
 }
